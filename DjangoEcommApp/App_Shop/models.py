c525f45de284c8241d817b297dbe6243a144571d
arsahak <arsahak99@gmail.com>
2022-05-01 01:21:33 +0600

    Done

--- DjangoEcommApp/App_Shop/models.py
+++ DjangoEcommApp/App_Shop/models.py
@@ -17,10 +17,8 @@ class Category(models.Model):
 class Product(models.Model):
     mainimage = models.ImageField(upload_to="Products")
     name = models.CharField(max_length=264)
-    category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name="category")
-    preview_text = models.TextField(
-        max_length=200, verbose_name="Preview Text")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
+    preview_text = models.TextField(max_length=200, verbose_name="Preview Text")
     detail_text = models.TextField(max_length=1000, verbose_name="Description")
     price = models.FloatField()
     old_price = models.FloatField(default=0.00)
